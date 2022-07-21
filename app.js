9ed21f22e960d135e9aea132d2ea59e17e14bf63
Fernando <fcortes16@alumnos.uaq.mx>
2022-07-21 01:25:45 -0500

    Webserver listo

---
+++ app.js
@@ -0,0 +1,51 @@
+
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express()
+const port = process.env.PORT;
+
+// const port= 8081;
+
+
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+//Middleware Servir contenido estatico
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('home',{
+      Nombre:'Fernando Cortes',
+      Titulo: 'Curso de node'
+  });
+});
+
+
+app.get('/generic', function (req, res) {
+  res.render('generic',{
+      Nombre:'Fernando Cortes',
+      Titulo: 'Generic'   
+
+  })
+});
+
+app.get('/elements', function (req, res) {
+  res.render('elements',{
+    Nombre:'Fernando Cortes',
+    Titulo: 'elements'   
+
+})
+});
+  
+
+app.get('*', (req, res)=>{
+    res.sendFile(__dirname +'/public/404.html');
+});
+
+app.listen(port,()=>{
+    console.log(`example app listening at http://localhost:${port}`);
+})
